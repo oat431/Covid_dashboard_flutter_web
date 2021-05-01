835ce8d4e713efcd3450413f6496b9fde022b3c3
Oralita <oat431@gmail.com>
2021-05-01 21:17:29 +0700

    [🔨 update] update from normal container to card

--- lib/component/content.dart
+++ lib/component/content.dart
@@ -7,21 +7,32 @@ class Content extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(30),
-      child: Row(
-        children: [
-          Text(
-            "$title: ",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
+      height: MediaQuery.of(context).size.height / 8,
+      width: MediaQuery.of(context).size.width / 1.5,
+      margin: EdgeInsets.all(15),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(25),
+          ),
+        ),
+        elevation: 1.5,
+        child: Row(
+          children: [
+            SizedBox(width: 15),
+            Text(
+              "$title: ",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
             ),
-          ),
-          Text(
-            "$data",
-            style: TextStyle(fontSize: 30),
-          ),
-        ],
+            Text(
+              "$data",
+              style: TextStyle(fontSize: 30),
+            ),
+          ],
+        ),
       ),
     );
   }

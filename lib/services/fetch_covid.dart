617712ed28626c2a917ea5f6daee7005e56e7968
Oralita <oat431@gmail.com>
2021-06-11 09:40:23 +0700

    [ 😎 update ] : api endpoint of covid-19

--- lib/services/fetch_covid.dart
+++ lib/services/fetch_covid.dart
@@ -5,7 +5,7 @@ import 'package:flutter_web_again/model/Covid.dart';
 
 Future<Covid> fetchCovid() async {
   final response =
-      await http.get(Uri.https('covid19.th-stat.com', 'api/open/today'));
+      await http.get(Uri.https('covid19.th-stat.com', 'json/covid19v2/getTodayCases.json'));
 
   if (response.statusCode == 200) {
     return Covid.fromJson(jsonDecode(response.body));
